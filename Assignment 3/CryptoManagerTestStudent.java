787bbead2f9509406ed8c2a4823300f3f7d7daf8
andycruz05 <149445786+andycruz05@users.noreply.github.com>
2024-03-11 22:19:58 -0400

    Add files via upload

---
+++ Assignment 3/CryptoManagerTestStudent.java
@@ -0,0 +1,54 @@
+/*
+ * Class: CMSC203 30339
+ * Instructor: Dr. Grinberg
+ * Description: CryptoManager 
+ * Due: 3/11/24
+ * Platform/compiler: Eclipse IDE
+ * I pledge that I have completed the programming  assignment independently. 
+*  I have not copied the code from a student or any source. 
+*  I have not given my code to any student.
+*  Print your Name here: Andy Cruz
+*/
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class CryptoManagerTestStudent {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void testIsStringInBounds() {
+		
+	}
+
+	@Test
+	void testCaesarEncryption() {
+		
+	}
+
+	@Test
+	void testBellasoEncryption() {
+	
+	}
+
+	@Test
+	void testCaesarDecryption() {
+		
+	}
+
+	@Test
+	void testBellasoDecryption() {
+		
+	}
+
+}
